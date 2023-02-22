96aa972ed22364e9bbee096dc61a211ea01c43a7
Jahongir-web <jahongirmh@gmail.com>
2023-02-22 12:38:45 +0500

    finished search products

--- src/controller/productCtrl.js
+++ src/controller/productCtrl.js
@@ -70,7 +70,6 @@ const productCtrl = {
     try {
       if(req.session.user_role === "001") {
         const file = req.files.image
-        // console.log(file);
 
         if(file.size > 20 * 1024 * 1024) {
           return res.status(401).json({message: "image's size large 20mb"})
@@ -171,6 +170,61 @@ const productCtrl = {
     }
   },
 
+  // Update Product
+
+  updateProduct: async (req, res) => {
+    
+    if(req.session.user_role === "001") {
+      try {
+        const {productId} = req.params
+        const newDesc = req.body.desc.reverse()[0];
+        req.body.desc = newDesc
+        const img = req.files
+        if(img !== null){
+          const file = req.files.image
+
+          if(file.size > 20 * 1024 * 1024) {
+            return res.status(401).json({message: "image's size large 20mb"})
+          }
+  
+          if(file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+            return res.status(401).json({message: "image format wrong!"})
+          }
+          
+          await cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "SSI Pro"}, async(err, result)=>{
+            if(err){
+              console.log(err);
+            }
+            removeTemp(file.tempFilePath)
+            req.body.image = {public_id: result.public_id, url: result.secure_url}
+          })
+        }
+        const product = await Product.findByIdAndUpdate(productId, req.body)
+        if(product) {
+          return res.status(200).send({message: "Product updated successfully"})
+        } else {
+          return res.status(404).send({message: "Product not found!"})
+        }
+      } catch (error) {
+        res.status(500).send({message: error.message})
+      }
+    } else {
+      res.status(401).send({message: "Not Allowed!"})
+    }
+  },
+
+  // Get product by Id
+  getProduct: async (req, res) => {
+    let {productId} = req.params
+
+    try {
+      const product = await Product.findById(productId)
+
+      res.status(200).send(product)
+    } catch (error) {
+      res.status(500).send({message: error.message})
+    }
+  },
 }
 
 
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,11 +2,14 @@ const router = require("express").Router()
 
 const productCtrl = require("../controller/productCtrl")
 
-router.get("/", productCtrl.getProducts)
 router.get("/api", productCtrl.getForClientProducts)
+router.get("/api/:productId", productCtrl.getProduct)
+
+router.get("/", productCtrl.getProducts)
 router.post("/add", productCtrl.addProduct)
-router.get("/add", productCtrl.addProductPage)
+router.get("/add/:productId", productCtrl.addProductPage)
 router.delete("/:productId", productCtrl.deleteProduct)
+router.put("/:productId", productCtrl.updateProduct)
 router.put("/promation/:productId", productCtrl.updateProductPromotion)
 router.put("/pubished/:productId", productCtrl.updateProductPublished)
 
